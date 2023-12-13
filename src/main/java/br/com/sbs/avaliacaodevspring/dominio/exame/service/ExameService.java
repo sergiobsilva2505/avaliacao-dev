4676c025cdb88ab44c4cb3200d758d345c6beb43
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-12-13 13:10:52 -0300

    refat exception handler

--- src/main/java/br/com/sbs/avaliacaodevspring/dominio/exame/service/ExameService.java
+++ src/main/java/br/com/sbs/avaliacaodevspring/dominio/exame/service/ExameService.java
@@ -1,13 +1,18 @@
-package br.com.sbs.avaliacaodevspring.exame;
+package br.com.sbs.avaliacaodevspring.dominio.exame.service;
 
-import br.com.sbs.avaliacaodevspring.exame.dto.ExameView;
-import br.com.sbs.avaliacaodevspring.exame.dto.NewExameForm;
-import br.com.sbs.avaliacaodevspring.exame.dto.UpdateExameForm;
+import br.com.sbs.avaliacaodevspring.dominio.exame.dto.NewExameForm;
+import br.com.sbs.avaliacaodevspring.dominio.exame.dto.ExameView;
+import br.com.sbs.avaliacaodevspring.dominio.exame.dto.UpdateExameForm;
+import br.com.sbs.avaliacaodevspring.dominio.exame.entity.Exame;
+import br.com.sbs.avaliacaodevspring.dominio.exame.repository.ExameRepository;
+import br.com.sbs.avaliacaodevspring.dominio.exame.service.exception.DatabaseException;
 import br.com.sbs.avaliacaodevspring.exception.ObjectNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,7 +32,7 @@ public class ExameService {
     }
 
     public Collection<ExameView> findAll() {
-        Collection<Exame> examesVo = exameRepository.findAll();
+        Collection<Exame> examesVo = (Collection<Exame>) exameRepository.findAll();
 
         return examesVo.stream().map(ExameView::new).collect(Collectors.toList());
     }
@@ -52,8 +57,13 @@ public class ExameService {
 
     @Transactional
     public void deleteById(Long id) {
-        Exame exame = exameRepository.getReferenceById(id);
-
-        exameRepository.delete(exame);
+//        Exame exame = exameRepository.getReferenceById(id);
+        try {
+            exameRepository.deleteById(id);
+        } catch (Exception ex) {
+//            throw new DatabaseException("Não pode ser deletado! Está associado a outros objetos");
+            System.out.println("=========> Estou no catch");
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/br/com/sbs/avaliacaodevspring/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/sbs/avaliacaodevspring/exception/GlobalExceptionHandler.java
@@ -1,18 +1,27 @@
 package br.com.sbs.avaliacaodevspring.exception;
 
+import br.com.sbs.avaliacaodevspring.dominio.exame.service.exception.DatabaseException;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class ViewExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView notFoundExaception(ObjectNotFoundException exception) {
+        ModelAndView modelAndView = new ModelAndView("exception/page404");
+        modelAndView.addObject("message", exception.getMessage());
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ModelAndView dataIntegrityExaception(DatabaseException exception) {
         ModelAndView modelAndView = new ModelAndView("exception/page404");
         modelAndView.addObject("message", exception.getMessage());
 
