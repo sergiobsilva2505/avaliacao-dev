982ec3a9890e5862a37908a59b3a944d886e2acd
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-12-14 12:47:30 -0300

    views adjustments

---
+++ src/main/java/br/com/sbs/avaliacaodevspring/dominio/realizado/validator/UpdateExameFuncionarioFormValidator.java
@@ -0,0 +1,31 @@
+package br.com.sbs.avaliacaodevspring.dominio.realizado.validator;
+
+import br.com.sbs.avaliacaodevspring.dominio.realizado.dto.UpdateExameFuncionarioForm;
+import br.com.sbs.avaliacaodevspring.dominio.realizado.repository.ExameFuncionarioRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UpdateExameFuncionarioFormValidator implements Validator {
+
+    private final ExameFuncionarioRepository exameFuncionarioRepository;
+
+    public UpdateExameFuncionarioFormValidator(ExameFuncionarioRepository exameFuncionarioRepository) {
+        this.exameFuncionarioRepository = exameFuncionarioRepository;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UpdateExameFuncionarioForm.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        UpdateExameFuncionarioForm form = (UpdateExameFuncionarioForm) target;
+
+//        if (exameFuncionarioRepository.existsByExameAndFuncionarioAndDateAndIdNot(form.exameId(), form.funcionarioId())) {
+//            errors.reject("exame.funcionario.exists.already");
+//        }
+    }
+}
